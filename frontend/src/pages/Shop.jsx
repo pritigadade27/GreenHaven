f824242cbf1154e95cb28ddf1d36b2647ad0808a
Priti Gadade <pritigadade27@gmail.com>
2026-08-06 03:57:41 +0530

    Delete frontend directory

--- frontend/src/pages/Shop.jsx
+++
@@ -1,5 +0,0 @@
-function Shop() {
-  return (<h1>Shop Page</h1>);
-}
-
-export default Shop;
--- frontend/src/pages/Wishlist.jsx
+++
@@ -1,5 +0,0 @@
-function Wishlist() {
-  return (<h1>Wishlist Page</h1>);
-}
-
-export default Wishlist;
